53a7aaf81ae3127f34c0bbbb87372843b849a6f1
gukhman <gukhman@gmail.com>
2024-10-31 12:34:15 +0200

    Новый проект

---
+++ app/src/main/java/com/example/a15_3_tablayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a15_3_tablayout
+
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupWindowInsets(R.id.main)
+        setupToolbar(R.id.toolbar, false)
+    }
+}
